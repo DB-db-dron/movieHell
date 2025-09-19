eef553d8c26f8acdcd750471c604f831ee739791
DRON BHATTACHARYA <dronbhattacharyadb@gmail.com>
2025-09-20 00:39:20 +0530

    fix: correct endpoint for movie search in fetchMovies function

--- services/api.ts
+++ services/api.ts
@@ -9,7 +9,7 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({query}: {query: string}) => {
     const endpoint = query?
-    `/movie?query=${encodeURIComponent(query)}`
+    `/search/movie?query=${encodeURIComponent(query)}`
     :`/discover/movie?sort_by=popularity.desc`;
     const res = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
         method: 'GET',
