cbaf1db16a7bcccf5ee3d5f44c4a0661e1e6f89a
DRON BHATTACHARYA <dronbhattacharyadb@gmail.com>
2025-09-19 22:26:15 +0530

    feat: add TMDB API configuration, fetchMovies function and useFetch hook

---
+++ services/api.ts
@@ -0,0 +1,28 @@
+export const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    }
+}
+
+export const fetchMovies = async ({query}: {query: string}) => {
+    const endpoint = query?
+    `/movie?query=${encodeURIComponent(query)}`
+    :`/discover/movie?sort_by=popularity.desc`;
+    const res = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    })
+    // console.error(res); // Debugging line
+
+    if(!res.ok) {
+        console.error(`fetchMovies-Error: ${res.status} - ${res.statusText}`);
+        throw new Error('Failed to fetch movies');
+    }
+    const data = await res.json();
+    // console.log('Movies data:', data); // Debugging line
+    return data.results;
+        
+}
