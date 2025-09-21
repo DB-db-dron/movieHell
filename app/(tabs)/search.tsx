fc07c9f5d5b9169808ab323af7c16d2498ec8756
DRON BHATTACHARYA <dronbhattacharyadb@gmail.com>
2025-09-21 19:01:03 +0530

    feat: implement search functionality with loading and error handling

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,20 +1,105 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import MovieCard from "@/components/movieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const {
+    data: movies,
+    loading: loadingMovies,
+    error: errorMovies,
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const func = async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    };
+    func();
+  }, [searchQuery]);
+
   return (
-    <View style={styles.container}>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          gap: 20,
+          paddingRight: 5,
+          marginBottom: 10,
+        }}
+        className="px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar
+                placeholderTxt="Search for a movie"
+                value={searchQuery}
+                onChangeTxt={(txt) => setSearchQuery(txt)}
+              />
+            </View>
+
+            {loadingMovies && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+            {errorMovies && (
+              <Text className="text-red-500 text-lg font-semibold px-5 my-3">
+                Error: {errorMovies?.message}
+              </Text>
+            )}
+            {!loadingMovies && !errorMovies && searchQuery.trim() && (
+              // && movies?.length! > 0
+              <Text className="text-white text-lg font-semibold">
+                Search Results for{" "}
+                <Text className="text-accent font-extrabold">
+                  {searchQuery}
+                </Text>
+              </Text>
+            )}
+          </>
+        }
+        ListEmptyComponent={
+          !loadingMovies && !errorMovies ? (
+            <View className="mt-10 px-5 items-center justify-center">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No movies found"
+                  : "Start typing to search for movies"}
+              </Text>
+            </View>
+          ) : null
+        }
+      />
     </View>
   );
 };
 
 export default Search;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
